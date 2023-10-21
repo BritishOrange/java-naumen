a4f3a02db1ec34dc599952ec1e30cf49561b0a16
SquirrelWithAvocado <aalxalx@bk.ru>
2023-10-21 15:40:54 +0500

    start

---
+++ src/main/java/com/develop/internetshop/InternetShopApplication.java
@@ -0,0 +1,13 @@
+package com.develop.internetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/internetshop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.internetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
